a12f54422ab1736d6e8439261924a41a2821356a
J_C <gomjan83@gmail.com>
2021-11-16 18:11:56 +0100

    PerfectNumbers Test finished

--- src/test/java/day02/numbers/PerfectNumbersMainTest.java
+++ src/test/java/day02/numbers/PerfectNumbersMainTest.java
@@ -1,6 +1,21 @@
 package day02.numbers;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PerfectNumbersMainTest {
+    PerfectNumbers numbers = new PerfectNumbers();
 
+    @Test
+    public void testPerfectNumber() {
+        boolean result = numbers.isPerfectNumber(6);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testNonPerfectNumber() {
+        boolean result = numbers.isPerfectNumber(9);
+        assertEquals(false, result);
+    }
 }
