dc73964b448e772e70040eb3587e7aa422f0334a
J_C <gomjan83@gmail.com>
2021-11-20 18:04:26 +0100

    Day04 Hangman excercise updated

--- src/main/java/day04/HangmanMain.java
+++ src/main/java/day04/HangmanMain.java
@@ -2,7 +2,7 @@ package day04;
 
 public class HangmanMain {
     public static void main(String[] args) {
-        Hangman hangman = new Hangman("automata", 10);
+        HangmanVersion2 hangman = new HangmanVersion2("automata", 10);
 
         hangman.runGame();
     }
